9cf54d61da131dbc25a29e3f79c87578ff9ae3f8
xiangdong.qu <qxd826@163.com>
2017-07-22 16:44:32 +0800

    可执行jar包打包测试

--- src/main/java/com/qxd/libra/App.java
+++ src/main/java/com/qxd/libra/App.java
@@ -1,13 +1,13 @@
 package com.qxd.libra;
 
+import com.qxd.core.utils.StringUtil;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(args[0]);
+        System.out.println(StringUtil.isChinese(args[0]));
     }
 }
